73744a9a4c663e798c7f29e6655b9910fe07ea1d
Nick Sidawy <nick.sidawy@gmail.com>
2020-12-19 17:09:38 -0500

    done

--- day17.2/src/main.rs
+++ day17.2/src/main.rs
@@ -11,31 +11,33 @@ struct Point {
 
 impl Point {
     pub fn get_neighbors(&self) -> Vec<Point>{
-        let neighbor_coords:Vec<Vec<i32>> = self.coords.iter()
-            .map(|ind| vec!(*ind, *ind + 1, *ind - 1)).collect();        
-
-        let mut curr_n: Vec<Vec<i32>> = Vec::new();
-        for nc in neighbor_coords {
-            let mut next_n: Vec<Vec<i32>> = Vec::new();
-            for n in nc {
-                if curr_n.len() == 0 {
-                    next_n.push(vec!(n));
-                    continue;
-                } 
-                for c in &curr_n {
-                    let mut x = c.clone();
-                    x.push(n);
-                    next_n.push(x);
+        self.coords.iter()
+            .map(|ind| vec!(*ind, *ind + 1, *ind - 1))
+            .fold(Vec::new(), |a: Vec<Vec<i32>>, nc| {
+                let mut next_n: Vec<Vec<i32>> = Vec::new(); 
+                for n in nc {
+                    if a.len() == 0 {
+                        next_n.push(vec!(n));
+                        continue;
+                    } 
+                    for c in a.iter() {
+                        let mut x = c.clone();
+                        x.push(n);
+                        next_n.push(x);
+                    }
                 }
-            } 
-            curr_n = next_n;
-        }
 
-        curr_n
-             .iter()
-             .map(|coords| Point {coords: coords.to_vec()})
-             .filter(|p| *p != *self)
-             .collect()
+                next_n
+            })
+            .iter()
+            .map(|coords| Point {coords: coords.to_vec()})
+            .filter(|p| *p != *self)
+            .collect()
+    }
+
+    pub fn is_active(&self, neighbors: &Vec<Point>, active_cubes: &HashSet<Point>) -> bool {
+        let count = neighbors.iter().fold(0u32, |s,p| s + if active_cubes.contains(p) {1} else {0});
+        (active_cubes.contains(self) && (count == 2 || count ==3)) || count == 3
     }
 }
 
@@ -58,25 +60,16 @@ fn step(active_cubes: HashSet<Point>, cycles: usize) -> HashSet<Point> {
         return active_cubes;
     }
     let mut next_cubes = HashSet::new();
-    let neighbors = active_cubes.iter().map(|a| a.get_neighbors());
-    for neighbor in neighbors {
+    let neighbors = active_cubes.iter().map(|a| (a, a.get_neighbors()));
+    for (a, neighbor) in neighbors {
+        if a.is_active(&neighbor, &active_cubes) {
+            next_cubes.insert(a.clone());
+        }
         for n in neighbor {
             let neighbors = n.get_neighbors();
-            let count = neighbors.iter().fold(0u32, |s,p| s + if active_cubes.contains(p) {1} else {0});
-            if active_cubes.contains(&n) {
-                if count == 2 || count ==3 {
-                    next_cubes.insert(n);
-                }
-            }  else if count == 3 {
+            if n.is_active(&neighbors, &active_cubes) {
                 next_cubes.insert(n);
             }
-        }
-    }
-    for a in active_cubes.iter() {
-        let neighbors = a.get_neighbors();
-        let count = neighbors.iter().fold(0u32, |s,p| s + if active_cubes.contains(p) {1} else {0});
-        if count == 2 || count ==3 {
-            next_cubes.insert(a.clone());
         }
     }
     step(next_cubes, cycles - 1)
