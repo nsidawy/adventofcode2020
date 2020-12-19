ac17a66540fa8071a7ef01f4b181739a4970bd4a
Nick Sidawy <nick.sidawy@gmail.com>
2020-12-19 11:38:54 -0500

    meh

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -35,19 +35,21 @@ fn step(cubes: Vec<Cube>, dimensions: Vec<usize>, dimension_widths: Vec<usize>, 
     if cycles == 0 {
         return cubes;
     }
-    println!("cycle {} {:?}", cycles, Local::now().format("%H:%M:%S").to_string());
+    println!("cycle {} {:?}", cycles, Local::now().format("%H:%M:%S.%f").to_string());
     let mut next_cubes = vec![Cube::Inactive; cubes.len()];
-    let active_cubes: Vec<(usize,Vec<usize>)> = cubes.iter().enumerate()
+    let active_cubes = cubes.iter().enumerate()
         .filter(|(_,c)| **c == Cube::Active)
-        .map(|(i,_)| (i, get_neighbors(i, &dimensions, &dimension_widths)))
-        .collect();
+        .map(|(i,_)| (i, get_neighbors(i, &dimensions, &dimension_widths)));
+    let mut count = 0;
     for (active_cube, neighbors) in active_cubes {
+        count += 1;
         next_cubes[active_cube] = get_new_state(&cubes, &neighbors, active_cube);
         for neighbor in neighbors {
             let neighbors = get_neighbors(neighbor, &dimensions, &dimension_widths);
             next_cubes[neighbor] = get_new_state(&cubes, &neighbors, neighbor);
         }
     }
+    println!("cycle {} {} {:?}", cycles, count, Local::now().format("%H:%M:%S.%f").to_string());
     step(next_cubes, dimensions, dimension_widths, cycles - 1)
 }
 
