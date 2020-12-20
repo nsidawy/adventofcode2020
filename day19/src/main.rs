a96d7330c14e8980587fb560b214b9f6e903b197
Nick Sidawy <nick.sidawy@gmail.com>
2020-12-20 10:23:45 -0500

    general solution

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -7,12 +7,11 @@ use regex::Regex;
 #[derive(Debug, Clone)]
 enum Rule {
     Reference(Vec<Vec<u32>>),
-    Recurse(Vec<u32>, Vec<u32>),
     Literal(char)
 }
 
 impl Rule {
-    pub fn parse(rule_id: u32, text: &str) -> Rule {
+    pub fn parse(text: &str) -> Rule {
         let literal_regex = Regex::new(r#""(\w)""#).expect("broken literal regex");
         if literal_regex.is_match(text) {
             let caps = literal_regex.captures(text).expect("could not capture literal");
@@ -34,122 +33,52 @@ impl Rule {
         }
         references.push(cur_reference);
 
-        if references.iter().any(|r| r.contains(&rule_id)) {
-            let base = references.iter().find(|r| !r.contains(&rule_id)).unwrap().clone();
-            let recurse = references.iter().find(|r| r.contains(&rule_id)).unwrap().clone();
-            return Rule::Recurse(base, recurse)
-        }
         Rule::Reference(references)
     }
 }
 
 fn main() {   
     let path = format!("{}\\input\\input.txt", env::current_dir().unwrap().to_str().unwrap()); 
-    let (mut rules, lines) = read_lines(path);
-    let rule_ids = rules.iter().map(|(k, _)| *k).collect();
-    blow_recurse(&rule_ids, &mut rules);
-    let valid_count = lines.iter().filter(|l| is_valid(0, l, &rules).0).count();
+    let (rules, lines) = read_lines(path);
+    let valid_count = lines.iter().filter(|l| is_valid(l, &rules)).count();
     println!("Part 1: {:#?}", valid_count);
 
     let path = format!("{}\\input\\input2.txt", env::current_dir().unwrap().to_str().unwrap()); 
-    let (mut rules, lines) = read_lines(path);
-    let rule_ids = rules.iter().map(|(k, _)| *k).collect();
-    blow_recurse(&rule_ids, &mut rules);
-    let valid_count = lines.iter().filter(|l| is_valid(0, l, &rules).0).count();
+    let (rules, lines) = read_lines(path);
+    let valid_count = lines.iter().filter(|l| is_valid(l, &rules)).count();
     println!("Part 2: {:#?}", valid_count);
 }
 
-fn blow_recurse(rule_ids: &Vec<u32>, rules: &mut HashMap<u32,Rule>) {
-    for rule_id in rule_ids {
-        let rule = rules.get(rule_id).unwrap();
-        match rule {
-            Rule::Reference(ref_list) => {
-                if !ref_list.iter().any(|rl| rl.iter()
-                    .any(|r| match rules.get(r).unwrap() { Rule::Recurse(_,_) => true, _ => false})){
-                        continue;
-                    }
-                    let ref_list = &ref_list[0];
-                    let mut new_list: Vec<Vec<u32>> = Vec::new();
-                    for r in ref_list {
-                        let rule_dep = rules.get(&r).unwrap();
-                        let refs = match rule_dep {
-                            Rule::Recurse(base, recurse) => {
-                                let mut recurse_refs: Vec<Vec<u32>> = Vec::new();
-                                for i in 0..5 {
-                                    let mut cur: Vec<u32> = recurse.clone();
-                                    for j in 0..i{
-                                        let mut next_cur = Vec::new();
-                                        for r1 in cur {
-                                            if r1 == *r {
-                                                for rc in recurse {
-                                                    next_cur.push(*rc);
-                                                }
-                                            } else {
-                                                next_cur.push(r1);
-                                            }
-                                        }
-                                        cur = next_cur;
-                                    }                                                                       
-                                    let index = cur.iter().position(|x| *x == *r).unwrap();
-                                    cur.remove(index);
-                                    recurse_refs.push(cur);
-                                }
-                                recurse_refs
-                            },
-                            _ => vec!(vec!(*r)) 
-                        };
-                        if new_list.len() == 0 {
-                            new_list = refs;
-                            continue;
-                        }
-                        let mut next_new_list: Vec<Vec<u32>> = Vec::new();
-                        for rf in refs {
-                            for l in new_list.iter() {
-                                let mut n = l.clone();
-                                n.extend(rf.clone());
-                                next_new_list.push(n);
-                            }
-                        }
-                        new_list = next_new_list;
-                    }
-                    rules.insert(*rule_id, Rule::Reference(new_list));
-            },
-            _ => ()
-        }
-    }
+fn is_valid(text: &String, rules: &HashMap<u32, Rule>) -> bool {
+   apply_rules(0, text, &rules).iter().any(|r| r == "")
 }
 
-fn is_valid(rule_id: u32, text: &String, rules: &HashMap<u32, Rule>) -> (bool, String) {
+fn apply_rules(rule_id: u32, text: &String, rules: &HashMap<u32, Rule>) -> Vec<String> {
     let rule  = rules.get(&rule_id).unwrap();
     match rule {
         Rule::Literal(c) => {
             let mut chars = text.chars();
             let c1 = chars.nth(0);
-            (c1.is_some() && c1.unwrap() == *c, String::from(chars.as_str())) 
+            if c1.is_some() && c1.unwrap() == *c {
+                return vec!(String::from(chars.as_str()));
+            }
+            vec!()
         },
         Rule::Reference(references) => {
+            let mut results = Vec::new();
             for reference_list in references {
-                let mut text1 = text.clone();
-                let mut success = true;
+                let mut texts = vec!(text.clone());
                 for reference in reference_list {
-                    let result = is_valid(*reference, &text1, rules);
-                    if !result.0 {
-                        success = false;
-                        break;
-                    }
-                    text1 = result.1;
+                     texts = texts.iter().flat_map(|t| apply_rules(*reference, &t, rules)).collect();
+                     if texts.len() == 0 {
+                         break;
+                     }                    
                 }
-                if success {
-                    if (rule_id == 0 && text1.len() == 0)
-                        || rule_id != 0 {
-                            return (true, String::from(text1.as_str()));
-                        }
-                }
+                results.extend(texts);
             }
 
-            (false, String::from(""))
+            results
         },
-        _ => (false, String::from(""))
     }
 }
 
@@ -167,7 +96,7 @@ fn read_lines(filename: String) -> (HashMap<u32, Rule>, Vec<String>) {
         let caps = rule_regex.captures(&line).unwrap();
         let rule_id = caps.get(1).unwrap().as_str().parse::<u32>().unwrap();
         let rule_text = caps.get(2).unwrap().as_str();
-        rules.insert(rule_id, Rule::parse(rule_id, &rule_text));
+        rules.insert(rule_id, Rule::parse(&rule_text));
     }
 
     (rules, lines.map(|l| l.unwrap()).filter(|l| l.len() > 0).collect())
