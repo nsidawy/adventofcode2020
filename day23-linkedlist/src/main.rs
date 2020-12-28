146471076b6a149e4106064d2123c19c22bf6a40
Nick Sidawy <nick.sidawy@gmail.com>
2020-12-28 15:33:41 -0500

    clean up

--- day23-linkedlist/src/main.rs
+++ day23-linkedlist/src/main.rs
@@ -1,6 +1,7 @@
 use std::rc::Rc;
-
 mod mylist;
+
+type CupList = mylist::CupList;
 
 fn main() {
     //let input = "389125467";
@@ -8,30 +9,27 @@ fn main() {
     let total = input.len();
     let (curr, next) = get_cups(input, total);
     let curr = play(curr, &next, 100);
-    println!("{}", curr.count());
     curr.print();
 
     let total = 1000000;
     let (curr, next) = get_cups(input, total);
+    let curr = play(curr, &next, 10000000);
     println!("{}", curr.count());
-    let _ = play(curr, &next, 10000000);
-    let start = Rc::clone(&(next[1]));
-    println!("{}", start.count());
-    let n1 = start.next().unwrap();
+    let n1 = next[1].next().unwrap();
     let n2 = n1.next().unwrap();
     let v1 = n1.value().unwrap();
     let v2 = n2.value().unwrap();
     println!("{}", v1*v2);
 }
 
-fn get_cups(input: &str, total: usize) -> (Rc<mylist::CupList>, Vec<Rc<mylist::CupList>>) {
+fn get_cups(input: &str, total: usize) -> (Rc<CupList>, Vec<Rc<CupList>>) {
     let ints: Vec<usize> = input.chars().map(|c| (c.to_digit(10).unwrap()) as usize).collect();
-    let mut next = vec![Rc::new(mylist::CupList::new_nil()); total + 1];
-    let mut current = Rc::new(mylist::CupList::new_nil());
+    let mut next = vec![Rc::new(CupList::new_nil()); total + 1];
+    let mut current = Rc::new(CupList::new_nil());
     let mut first = None;
     for i in (ints.len()+1..total+1).rev().chain((0..ints.len()).rev()) {
         let value = if i < ints.len() { ints[i] } else { i };
-        current = Rc::new(mylist::CupList::new(value, &current));
+        current = Rc::new(CupList::new(value, &current));
         next[value] = Rc::clone(&current);
         if first.is_none() {
             first = Some(Rc::clone(&current));
@@ -41,7 +39,7 @@ fn get_cups(input: &str, total: usize) -> (Rc<mylist::CupList>, Vec<Rc<mylist::C
     (current, next)
 }
 
-fn play(mut current: Rc<mylist::CupList>, next: &Vec<Rc<mylist::CupList>>, moves: u32) -> Rc<mylist::CupList> {
+fn play(mut current: Rc<CupList>, next: &Vec<Rc<CupList>>, moves: u32) -> Rc<CupList> {
     for _ in 0..moves {
         //1.
         let cup1 = current.next().unwrap();
