1bb9651cf110921cbcba78f1e03966db85fabb38
Nick Sidawy <nick.sidawy@gmail.com>
2020-12-28 16:46:49 -0500

    rename

--- day23-linkedlist/src/main.rs
+++ day23-linkedlist/src/main.rs
@@ -7,15 +7,16 @@ fn main() {
     //let input = "389125467";
     let input = "123487596";
     let total = input.len();
-    let (curr, next) = get_cups(input, total);
-    let curr = play(curr, &next, 100);
+    let (curr, cup_pointers) = get_cups(input, total);
+    curr.print();
+    let curr = play(curr, &cup_pointers, 100);
     curr.print();
 
     let total = 1000000;
-    let (curr, next) = get_cups(input, total);
-    let curr = play(curr, &next, 10000000);
+    let (curr, cup_pointers) = get_cups(input, total);
+    let curr = play(curr, &cup_pointers, 10000000);
     println!("{}", curr.count());
-    let n1 = next[1].next().unwrap();
+    let n1 = cup_pointers[1].next().unwrap();
     let n2 = n1.next().unwrap();
     let v1 = n1.value().unwrap();
     let v2 = n2.value().unwrap();
@@ -24,22 +25,24 @@ fn main() {
 
 fn get_cups(input: &str, total: usize) -> (Rc<CupList>, Vec<Rc<CupList>>) {
     let ints: Vec<usize> = input.chars().map(|c| (c.to_digit(10).unwrap()) as usize).collect();
-    let mut next = vec![Rc::new(CupList::new_nil()); total + 1];
+    let mut cup_pointers = vec![Rc::new(CupList::new_nil()); total + 1];
     let mut current = Rc::new(CupList::new_nil());
-    let mut first = None;
-    for i in (ints.len()+1..total+1).rev().chain((0..ints.len()).rev()) {
-        let value = if i < ints.len() { ints[i] } else { i };
+    let mut tail = None;
+    let ascending_start = ints.len()+1;
+    let labels = ints.into_iter().chain(ascending_start..total+1);
+    for value in labels.rev() {
         current = Rc::new(CupList::new(value, &current));
-        next[value] = Rc::clone(&current);
-        if first.is_none() {
-            first = Some(Rc::clone(&current));
+        cup_pointers[value] = Rc::clone(&current);
+        if tail.is_none() {
+            tail = Some(Rc::clone(&current));
         }
     }
-    first.unwrap().set_next(&current);
-    (current, next)
+    // create circular reference
+    tail.unwrap().set_next(&current);
+    (current, cup_pointers)
 }
 
-fn play(mut current: Rc<CupList>, next: &Vec<Rc<CupList>>, moves: u32) -> Rc<CupList> {
+fn play(mut current: Rc<CupList>, cup_pointers: &Vec<Rc<CupList>>, moves: u32) -> Rc<CupList> {
     for _ in 0..moves {
         //1.
         let cup1 = current.next().unwrap();
@@ -52,16 +55,16 @@ fn play(mut current: Rc<CupList>, next: &Vec<Rc<CupList>>, moves: u32) -> Rc<Cup
         let c1 = cup1.value().unwrap();
         let c2 = cup2.value().unwrap();
         let c3 = cup3.value().unwrap();
-        let mut destination = if c == 1 { next.len()-1 } else { c - 1 }; 
+        let mut destination = if c == 1 { cup_pointers.len()-1 } else { c - 1 }; 
         while destination == c1 || destination == c2 || destination == c3 {
-            destination = if destination == 1 { next.len()-1 } else { destination - 1 }; 
+            destination = if destination == 1 { cup_pointers.len()-1 } else { destination - 1 }; 
         };
 
         //3.
-        let destination = Rc::clone(&next[destination]);
+        let destination = Rc::clone(&cup_pointers[destination]);
         cup3.set_next(&destination.next().unwrap());
         destination.set_next(&cup1);
-
+        
         current = current.next().unwrap();
     }
 
